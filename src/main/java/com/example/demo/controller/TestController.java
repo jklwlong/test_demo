be20f53262a5d1d6d6db777022c869f49e59d502
unknown <>
2018-10-21 23:18:10 +0800

    log

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/")
     public String Test(){
+        logger.info("-------------------------------------test controller");
         return "test_demo111";
     }
 }
